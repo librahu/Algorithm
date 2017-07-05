3261fd9602799c251cb6d50c52a5e1b2e2259753
librahu <879122659@qq.com>
2017-07-06 00:39:57 +0800

    add leetcode_21

---
+++ leetcode/leetcode_21.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ /**
+  * time : O(m+n) , m,n is the length of l1 ans l2
+  * space : O(1)
+  */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* res = new ListNode(0);
+        ListNode* cur = res;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        if(!l1)
+            cur->next = l2;
+        else
+            cur->next = l1;
+        return res->next;
+    }
+};
