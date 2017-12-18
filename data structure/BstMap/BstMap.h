77ab9c373778b94d6230aac328d3876f48fc2f51
librahu <879122659@qq.com>
2017-12-18 15:27:53 +0800

    modify BstMap.h

--- data structure/BstMap/BstMap.h
+++ data structure/BstMap/BstMap.h
@@ -11,7 +11,7 @@ class Node{
 		Node<K, V>* right;
 		int n; //以该节点为根的子树的节点数目 
 		
-		Node(K _key, V _val, int _n) : key(_key), val(_val), n(_n){}
+		Node(K _key, V _val, int _n) : key(_key), val(_val), n(_n), left(NULL), right(NULL){}
 };
 
 template<typename K, typename V>
@@ -20,11 +20,17 @@ class BstMap{
 		Node<K, V>* root;
 		
 		int size(Node<K, V>* x);
-		
+		int get(Node<K, V>* x, K key);
+		Node<K, V>* put(Node<K, V>* x, K key, V val);
 	public:
 		BstMap();
 		~BstMap();
+		//返回节点数 
 		int size();
+		//查找 
+		V get(K key);
+		//插入
+		void put(K key, V val); 
 		
 };
 
@@ -49,6 +55,35 @@ int BstMap<K, V>::size(Node<K, V>* x){
 template<typename K, typename V>
 int BstMap<K, V>::size(){
 	return size(root);
+}
+
+template<typename K, typename V>
+V BstMap<K, V>::get(Node<K, V>* x, K key){
+	if(x == NULL)	return -1;
+	if(key < x->key)	return get(x->left, key);
+	else if(key > x->key) return get(x->right, key);
+	return x->val;
+}
+
+template<typename K, typename V>
+V BstMap<K, V>::get(K key){
+	return get(root, key);
+} 
+
+template<typename K, typename V>
+Node<K, V>* BstMap<K, V>::put(Node<K, V>* x, K key, V val){
+	if(x == NULL)	return new Node(key, val, 1);
+	
+	if(key < x->key)	x->left = put(x->left, key, val);
+	else if(key > x->key) x->right = put(x->rigth, key, val);
+	else x->val = val;
+	x->N = size(x->left) + size(x->right) + 1;
+	return x; 
+}
+
+template<typename K, typename V>
+void BstMap<K, V>::put(K key, V val){
+	root = put(root, key, val);
 }
 
 #endif 
